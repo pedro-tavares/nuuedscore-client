a837a37dffd82904e7f5dbfa7a381802a7448799
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-03-08 17:34:41 +0100

    Scores

---
+++ src/com/nuuedscore/client/ui/ScoresPanel.java
@@ -0,0 +1,42 @@
+package com.nuuedscore.client.ui;
+
+import com.google.gwt.user.client.ui.Label;
+import com.nuuedscore.shared.dto.refdata.RefScore;
+
+/**
+ * Scores Panel
+ * 
+ * @author PATavares
+ * @since Mar 2021
+ * 
+ */
+public class ScoresPanel extends TitledPanel {
+	
+	private RefScore REF_SCORE;
+	
+	public ScoresPanel(RefScore refScore) {
+		super(refScore.value() + " SCORES");
+		this.REF_SCORE = refScore;
+		
+		this.titleLabel.setStyleName("subTitledPanelLabel");
+		this.setSpacing(10);
+		
+		init();
+	}
+	
+	private void init() {
+		switch (REF_SCORE) {
+		case HIGH:
+			this.setStyleName("highTitledPanel");
+			break;
+		case LOW:
+			this.setStyleName("lowTitledPanel");			
+			break;
+		default:
+			break;
+		}
+		
+		this.add(new Label(this.titleLabel.getText() + " content"));
+	}
+
+}
---
+++ src/com/nuuedscore/shared/dto/refdata/RefScore.java
@@ -0,0 +1,34 @@
+package com.nuuedscore.shared.dto.refdata;
+
+/**
+ * Score enum 
+ * Constants that classify a Score
+ * 
+ * @author PATavares
+ * @since Feb 2021
+ * 
+ */
+public enum RefScore {
+	
+	LOW("LOW"), 
+	HIGH("HIGH");
+
+	private String value;
+
+	RefScore(String value) {
+		this.value = value;
+	}
+
+	public String value() {
+		return value;
+	}
+	
+	public static RefScore get(String value) {
+		for(RefScore r: RefScore.values()) {
+			if (r.value.equals(value)) {
+				return r;
+			}
+		}
+		return null;
+	}
+}
