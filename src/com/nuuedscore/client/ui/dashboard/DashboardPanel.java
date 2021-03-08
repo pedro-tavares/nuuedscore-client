8ef43756ec26349b50aacfc79b01cdb48d9c9282
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-03-08 17:35:31 +0100

    Dashboard

--- src/com/nuuedscore/client/ui/dashboard/DashboardPanel.java
+++ src/com/nuuedscore/client/ui/dashboard/DashboardPanel.java
@@ -1,7 +1,10 @@
 package com.nuuedscore.client.ui.dashboard;
 
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.nuuedscore.client.NuuEdScore;
+import com.nuuedscore.client.ui.ScoresPanel;
 import com.nuuedscore.client.ui.TitledPanel;
+import com.nuuedscore.shared.dto.refdata.RefScore;
 
 /**
  * Dashboard Panel
@@ -12,6 +15,11 @@ import com.nuuedscore.client.ui.TitledPanel;
  */
 public class DashboardPanel extends TitledPanel {
 
+	private HorizontalPanel innerPanel = new HorizontalPanel();
+	private AptitudePanel lowAptitudePanel = new AptitudePanel(RefScore.LOW);
+	private SkillsMasterPanel skillsMasterPanel = new SkillsMasterPanel();
+	private AptitudePanel highAptitudePanel = new AptitudePanel(RefScore.HIGH);
+	
 	public DashboardPanel() {
 		super(NuuEdScore.GET_USER().getFirstName() +  "'s Dashboard");
 
@@ -21,5 +29,10 @@ public class DashboardPanel extends TitledPanel {
 	}
 
 	private void init() {
+		innerPanel.add(lowAptitudePanel);
+		innerPanel.add(skillsMasterPanel);
+		innerPanel.add(highAptitudePanel);
+		
+		this.add(innerPanel);
 	}
 }
