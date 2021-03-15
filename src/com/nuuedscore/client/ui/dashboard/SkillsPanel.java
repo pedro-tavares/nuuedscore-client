ad44ecf5b7ab42fad22ce55b335547842cbec156
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-03-15 11:36:52 +0100

    SkillsPanel using mapped GaugePanel

--- src/com/nuuedscore/client/ui/dashboard/SkillsPanel.java
+++ src/com/nuuedscore/client/ui/dashboard/SkillsPanel.java
@@ -21,18 +21,13 @@ public class SkillsPanel extends VerticalPanel {
 	private Button skillsButton;
 	private HorizontalPanel innerPanel = new HorizontalPanel();
 	private VerticalPanel 
-		gaugePanel = new VerticalPanel(),
+		gaugePanel,
 		skillsLegendPanel;
-	private Image 
-		gaugeSoftSkillsHighImage,
-		gaugeSoftSkillsLowImage,
-		gaugeHardSkillsHighImage,
-		gaugeHardSkillsLowImage;
-	private Label 
-		softSkillsHighLabel,
-		softSkillsLowLabel,
-		hardSkillsHighLabel,
-		hardSkillsLowLabel;
+	private GaugePanel 
+		skillsSoftHighGaugePanel = new GaugePanel(RefSkill.SOFT, RefScore.HIGH),
+		skillsSoftLowGaugePanel = new GaugePanel(RefSkill.SOFT, RefScore.LOW),
+		skillsHardHighGaugePanel = new GaugePanel(RefSkill.HARD, RefScore.HIGH),
+		skillsHardLowGaugePanel = new GaugePanel(RefSkill.HARD, RefScore.LOW);
 	
 	public SkillsPanel(RefSkill refSkill) {
 		this.REF_SKILL = refSkill;
@@ -44,59 +39,33 @@ public class SkillsPanel extends VerticalPanel {
 	}
 	
 	private void init() {		
+		gaugePanel = new VerticalPanel();
+		
 		switch (REF_SKILL) {
 		case SOFT:
 			skillsButton = new Button("SOFT SKILLS");
+			skillsButton.setStyleName("gwt-Button-grey");
+			skillsButton.setWidth("100%");
 			this.add(skillsButton);
-			// HIGH
-			gaugeSoftSkillsHighImage = new Image("images/graphx/gauges/Soft-Gauge1.svg");
-			gaugeSoftSkillsHighImage.setStyleName("gaugeImage");
-			gaugePanel.add(gaugeSoftSkillsHighImage);
-			softSkillsHighLabel = new Label(RefScore.HIGH.value());
-			softSkillsHighLabel.setStyleName("skillsLabel");
-			softSkillsHighLabel.setHorizontalAlignment(ALIGN_CENTER);
-			gaugePanel.add(softSkillsHighLabel);
-			// LOW
-			gaugeSoftSkillsLowImage = new Image("images/graphx/gauges/Soft-Gauge2.svg");
-			gaugeSoftSkillsLowImage.setStyleName("gaugeImage");
-			gaugePanel.add(gaugeSoftSkillsLowImage);
-			softSkillsLowLabel = new Label(RefScore.LOW.value());
-			softSkillsLowLabel.setStyleName("skillsLabel");
-			softSkillsLowLabel.setHorizontalAlignment(ALIGN_CENTER);
-			gaugePanel.add(softSkillsLowLabel);
-			// SKILLS LEGEND
-			skillsLegendPanel = new SkillsLegendPanel(this.REF_SKILL);
+			gaugePanel.add(skillsSoftHighGaugePanel);
+			gaugePanel.add(skillsSoftLowGaugePanel);
 			break;
 		case HARD:
 			skillsButton = new Button("HARD SKILLS");
+			skillsButton.setStyleName("gwt-Button-grey");
+			skillsButton.setWidth("100%");
 			this.add(skillsButton);
-			// HIGH
-			gaugeHardSkillsHighImage = new Image("images/graphx/gauges/Hard-Gauge1.svg");
-			gaugeHardSkillsHighImage.setStyleName("gaugeImage");
-			gaugePanel.add(gaugeHardSkillsHighImage);
-			hardSkillsHighLabel = new Label(RefScore.HIGH.value());
-			hardSkillsHighLabel.setStyleName("skillsLabel");
-			hardSkillsHighLabel.setHorizontalAlignment(ALIGN_CENTER);
-			gaugePanel.add(hardSkillsHighLabel);
-			// LOW
-			gaugeHardSkillsLowImage = new Image("images/graphx/gauges/Hard-Gauge2.svg");
-			gaugeHardSkillsLowImage.setStyleName("gaugeImage");
-			gaugePanel.add(gaugeHardSkillsLowImage);
-			hardSkillsLowLabel = new Label(RefScore.HIGH.value());
-			hardSkillsLowLabel.setStyleName("skillsLabel");
-			hardSkillsLowLabel.setHorizontalAlignment(ALIGN_CENTER);
-			gaugePanel.add(hardSkillsLowLabel);			
-			// SKILLS LEGEND
-			skillsLegendPanel = new SkillsLegendPanel(this.REF_SKILL);
+			gaugePanel.add(skillsHardHighGaugePanel);
+			gaugePanel.add(skillsHardLowGaugePanel);			
 			break;
 		default:
 			break;
 		}
 		
-		skillsButton.setStyleName("gwt-Button-grey");
-		skillsButton.setWidth("100%");
+		skillsLegendPanel = new SkillsLegendPanel(this.REF_SKILL);
 
 		this.add(innerPanel);
+		
 		innerPanel.add(gaugePanel);
 		innerPanel.add(skillsLegendPanel);
 	}
