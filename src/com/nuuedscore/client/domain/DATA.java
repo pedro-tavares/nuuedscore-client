95820f07b7028a221d58ac0dfe5f9ebe8b01b2a5
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-03-26 04:41:41 +0100

    Student Resource DATA

---
+++ src/com/nuuedscore/client/domain/DATA.java
@@ -0,0 +1,18 @@
+package com.nuuedscore.client.domain;
+
+import java.util.List;
+
+import com.nuuedscore.shared.dto.StudentResource;
+
+/**
+ * DATA
+ * 
+ * @author PATavares
+ * @since Feb 2021
+ * 
+ */
+public class DATA {
+
+	public static List<StudentResource> STUDENT_RESOURCES;
+	
+}
