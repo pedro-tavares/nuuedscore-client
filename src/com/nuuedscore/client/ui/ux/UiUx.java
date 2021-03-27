b5adc2a4cf9f0774fcb0a6e16ae688e48df28f45
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-03-27 16:02:53 +0100

    Update UiUx

--- src/com/nuuedscore/client/ui/ux/UiUx.java
+++ src/com/nuuedscore/client/ui/ux/UiUx.java
@@ -2,6 +2,7 @@ package com.nuuedscore.client.ui.ux;
 
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Image;
+import com.nuuedscore.shared.dto.refdata.RefTopic;
 
 /**
  * UI UX
@@ -15,8 +16,11 @@ public class UiUx {
 	/**
 	 * static bindings
 	 */
-	static public Image imageForTopic(String topic) {
-		return new Image("/images/topics/" + topic + ".png"); 
+	static public Image imageForTopic(RefTopic topic) {
+		switch(topic) {
+		case Academics: return new Image("/images/topics/" + topic + ".png");
+		default: return new Image("/images/NuLogo_Small.jpg");
+		}
 	}
 	
 	static public String colourForSubject(String subject) {
