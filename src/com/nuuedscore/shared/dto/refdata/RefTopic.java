580195ea33504f82ec2edc24589559c6284af38c
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-03-27 17:52:35 +0100

    RefTopic

--- src/com/nuuedscore/shared/dto/refdata/RefTopic.java
+++ src/com/nuuedscore/shared/dto/refdata/RefTopic.java
@@ -14,7 +14,18 @@ public enum RefTopic {
 	
 	Academics("Academics"), 
 	Financial_Literacy("Financial Literacy"),
-	Hard_Skills("Hard Skills");
+	Hard_Skills("Hard Skills"),
+	Soft_Skills("Soft Skills"),
+	Practice_Civil_Service_Exam("Practice Civil Service Exam"),
+	Tutoring_Options("Tutoring Options"),
+	Personal_Growth("Personal Growth"),
+	HBCUs("HBCUs"),
+	College_Prep_Exams("College Prep Exams"),
+	Practice_College_Exams_PSAT_ACT_SAT("Practice College Exams - PSAT/ACT/SAT"),
+	Practice_College_Exams_K_12_Academic_Assessments("Practice College Exams - K-12 Academic Assessments"),
+	College_Alternatives_Entrepreneurship("College Alternatives - Entrepreneurship"),
+	Military_Service_US_Defense("Military Service - US Defense"),
+	California_Common_Core_Standards("California Common Core Standards");
 
 	private String value;
 
