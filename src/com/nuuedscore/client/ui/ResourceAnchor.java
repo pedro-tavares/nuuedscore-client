82c7dbeaaf82d26a218059cfa5a08f29b2fe8953
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-04-05 00:21:14 +0200

    Introduce ResourceAnchor

---
+++ src/com/nuuedscore/client/ui/ResourceAnchor.java
@@ -0,0 +1,28 @@
+package com.nuuedscore.client.ui;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Label;
+
+/**
+ * Resource Anchor
+ * 
+ * @author PATavares
+ * @since Apr 2021
+ * 
+*/
+public class ResourceAnchor extends Label {
+
+	private String resource;
+	
+	public ResourceAnchor(String name, String resource) {
+		super(name);
+		this.resource = resource;
+		
+		this.setStyleName("resourceAnchor");
+		this.addClickHandler(event -> {
+			//Window.alert(this.resource);
+			// TODO can do features on resource
+			Window.open(this.resource, this.getText(), "");
+		});
+	}
+}
